986080f910e1792af6d5e2f8941478cb3d6cbc85
Chirag Arora <arora2002chirag@gmail.com>
2023-10-31 21:21:07 +0530

    Initial Commit

---
+++ StartoonLabstest/StartoonLabstestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartoonLabstestApp.swift
+//  StartoonLabstest
+//
+//  Created by chirag arora on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StartoonLabstestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
